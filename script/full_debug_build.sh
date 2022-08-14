7b0a9f76ae35450f547f690dfb78d09e485fe08e
Alex Robenko <arobenko@gmail.com>
2022-08-14 12:55:35 +1000

    Extra build scripts.

---
+++ script/full_debug_build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "${CC}" -o -z "$CXX" ]; then
+    echo "ERROR: Compilers are not provided"
+    exit 1
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR=$( dirname ${SCRIPT_DIR} )
+export BUILD_DIR="${ROOT_DIR}/build.full.${CC}"
+export COMMON_INSTALL_DIR=${BUILD_DIR}/install
+export COMMON_BUILD_TYPE=Debug
+mkdir -p ${BUILD_DIR}
+
+${SCRIPT_DIR}/prepare_externals.sh
+
+cd ${BUILD_DIR}
+cmake .. -DCMAKE_INSTALL_PREFIX=${COMMON_INSTALL_DIR} \
+    -DCMAKE_BUILD_TYPE=Debug -DUBLOX_BUILD_GEN_TEST=ON \
+    -DUBLOX_BUILD_GEN_TOOLS=ON "$@"
+
+procs=$(nproc)
+if [ -n "${procs}" ]; then
+    procs_param="--parallel ${procs}"
+fi
+
+cmake --build ${BUILD_DIR} --config ${COMMON_BUILD_TYPE} --target install ${procs_param}
